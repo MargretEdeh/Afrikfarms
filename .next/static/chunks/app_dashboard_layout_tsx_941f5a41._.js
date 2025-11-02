657f2540d058ec62a42c4c8ed77947e5f867652c
margretedeh <sagaedeh@gmail.com>
2025-11-02 12:03:27 +0100

    build

--- .next/static/chunks/app_dashboard_layout_tsx_941f5a41._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_61f23ecb._.js",
-  "static/chunks/app_dashboard_12cccef6._.js"
-],
-    source: "dynamic"
-});
