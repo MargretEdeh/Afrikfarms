e83aab97c02c21e1fafd147c7a4face5f78ebbd3
Valexjnr54 <jimvalex54@gmail.com>
2025-11-26 16:03:13 +0100

    build fix

--- .next/static/chunks/app_dashboard_farmers_page_tsx_7f1626a8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_d71d6a89._.js",
-  "static/chunks/node_modules_f38b23bc._.js"
-],
-    source: "dynamic"
-});
